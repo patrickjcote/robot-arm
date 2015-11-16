b99c5716270dde2fdad968a62126952314e9b5c5
pcote <pcote@mtech.edu>
2015-11-15 19:18:14 -0700

    Added serial com control

--- main.c
+++ main.c
@@ -1,51 +1,105 @@
 #include  <msp430.h>
 
+void initialize();
+void testInput(char inChar);
+void messageOut(char messageIn[], int length);
+
+volatile int servoPosition;
+
 int main(void)
 {
 	volatile int i;
 
-	WDTCTL = WDTPW + WDTHOLD;
+	initialize();
 
-	P1DIR |= BIT0; 							// Internal LEDs P1.0 of Launchpad is output
-
-	P1DIR |= BIT6;							// P1.6/TA0.1 is used for PWM, thus also an output -> servo 1
-    P2DIR |= BIT2;	                        // P2.2/TA1.1 is used for PWM, thus also an output -> servo 2
-
-	P1OUT = 0; 								// Clear all outputs P1
-	P2OUT = 0; 								// Clear all outputs P2
-
-	P1SEL |= BIT6;                          // P1.6 select TA0.1 option
-	P2SEL |= BIT2;                          // P2.2 select TA1.1 option
-
-    //SMCLOCK is 1MHz so 22222 is about 20ms PWM Period
-	TA0CCR0 = 22222;                           // PWM Period TA0.1
-	TA1CCR0 = 22222;                           // PWM Period TA1.1
-
-	// setting 1500 is 1.5ms is 0deg. servo pos
-	TA0CCR1 = 1500;                            // CCR1 PWM duty cycle
-	TA1CCR1 = 1500;                            // CCR1 PWM duty cycle
-
-	TA0CCTL1 = OUTMOD_7;                       // CCR1 reset/set
-	TA0CTL   = TASSEL_2 + MC_1;                // SMCLK, up mode
-	TA1CCTL1 = OUTMOD_7;                       // CCR1 reset/set
-	TA1CTL   = TASSEL_2 + MC_1;                // SMCLK, up mode
+	__bis_SR_register(GIE);
 
 	while(1){
-		for (i = 900; i < 2000; i++)
-		{
-			TA0CCR1 = i;
-			TA1CCR1 = i;
-			__delay_cycles(2000);
-		}//for
-		for (i = 2000; i > 900; i--)
-		{
 
-			TA0CCR1 = i;
-			TA1CCR1 = i;
-			__delay_cycles(2000);
+	}//
 
-		}//for
-	}//while
-}//main()
+}
 
-//main.c
+void initialize(){
+
+	WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+
+
+	//--------------------- Serial Comm Setup -------------------------
+	if (CALBC1_1MHZ==0xFF)					// If calibration constant erased
+	{
+		while(1);                               // do not load, trap CPU!!
+	}
+	DCOCTL = 0;                               // Select lowest DCOx and MODx settings
+	BCSCTL1 = CALBC1_1MHZ;                    // Set DCO
+	DCOCTL = CALDCO_1MHZ;
+	P1SEL = BIT1 + BIT2 ;                     // P1.1 = RXD, P1.2=TXD
+	P1SEL2 = BIT1 + BIT2 ;                    // P1.1 = RXD, P1.2=TXD
+	UCA0CTL1 |= UCSSEL_2;                     // SMCLK
+	UCA0BR0 = 104;                            // 1MHz 9600
+	UCA0BR1 = 0;                              // 1MHz 9600
+	UCA0MCTL = UCBRS0;                        // Modulation UCBRSx = 1
+	UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+	IE2 |= UCA0RXIE;                          // Enable USCI_A0 RX interrupt
+
+	//---------------------- Servo PWM Setup ----------------------------
+
+	P2DIR |= 0x0F;							// P2.2/TA1.1 is used for PWM, thus also an output -> servo 2
+	P2OUT = 0; 								// Clear all outputs P2
+	P2SEL |= BIT1 + BIT2;                          // P2.2 select TA1.1 option
+	TA1CCR0 = 22222;                        // PWM Period TA1.1 of 20ms
+	TA1CCR1 = 1500;                         // CCR1 PWM duty cycle
+	TA1CCTL1 = OUTMOD_7;                    // CCR1 reset/set
+	TA1CTL   = TASSEL_2 + MC_1;             // SMCLK, up mode
+
+}//setRegisters()
+
+#pragma vector=USCIAB0RX_VECTOR
+//  Echo back RXed character, confirm TX buffer is ready first
+__interrupt void USCI0RX_ISR(void)
+{
+	volatile char inChar;
+	inChar = UCA0RXBUF;
+	testInput(inChar);
+}
+
+void testInput(char inChar){
+
+	switch(inChar){
+
+	case 's':
+		servoPosition += 50;
+		break;
+	case 'w':
+		servoPosition -= 50;
+		break;
+	default:
+		messageOut("Invalid Key",11);
+		break;
+	}//switch
+
+	if(servoPosition < 900){
+		servoPosition = 900;
+		messageOut("Hit High Edge",13);
+	}
+	if(servoPosition > 2000){
+		servoPosition=2000;
+		messageOut("Hit Low Edge",12);
+	}
+	TA1CCR1 = servoPosition;
+}//testInput()
+
+void messageOut(char messageIn[], int length){
+
+	volatile int i;
+	for(i = 0; i < length; i++){
+		while (!(IFG2&UCA0TXIFG));                // USCI_A0 TX buffer ready?
+		UCA0TXBUF = messageIn[i];
+	}//for
+
+	while (!(IFG2&UCA0TXIFG));                // USCI_A0 TX buffer ready?
+	UCA0TXBUF = 0x0A;
+	while (!(IFG2&UCA0TXIFG));                // USCI_A0 TX buffer ready?
+	UCA0TXBUF = 0x0D;
+
+}//outputMessage
