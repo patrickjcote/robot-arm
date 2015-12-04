1cb409356a09f64054b333a7365c500807332cf7
pcote <pcote@mtech.edu>
2015-12-04 15:53:48 -0700

    Added defined directions to header

--- robotarm.h
+++ robotarm.h
@@ -5,12 +5,14 @@
 #define PWM_PERIOD 20000
 #define UP 50
 #define DOWN -50
+#define LEFT 50
+#define RIGHT -50
 
 //Prototypes
 void initialize();
 void testInput(char inChar);
 void messageOut(char messageIn[], int length);
-void moveServo(int servo, int upDown);
+void moveServo(int servoNumber, int direction);
 
 //Global
 extern volatile int servoPosition[4];
